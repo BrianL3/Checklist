49b26617f93b46b93d2df3fb2a5946cb597bf513
BrianL3 <Brian.James.Ledbetter@gmail.com>
2014-12-30 14:32:07 -0800

    went through the first 2 sections of the book

---
+++ Checklists/Checklists/ChecklistItem.swift
@@ -0,0 +1,9 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Brian Ledbetter on 12/30/14.
+//  Copyright (c) 2014 Brian Ledbetter. All rights reserved.
+//
+
+import Foundation
---
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ChecklistViewController.swift
+//  Checklists
+//
+//  Created by Brian Ledbetter on 12/30/14.
+//  Copyright (c) 2014 Brian Ledbetter. All rights reserved.
+//
+
+import Foundation
